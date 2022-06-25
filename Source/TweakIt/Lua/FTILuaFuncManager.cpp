6ddc6155c90af3399e3637d5f6ed731879d42f6e
Feyko <feykook@gmail.com>
2022-06-25 21:43:49 +0100

    feat: lua func load/dump

--- Source/TweakIt/Lua/FTILuaFuncManager.cpp
+++ Source/TweakIt/Lua/FTILuaFuncManager.cpp
@@ -1,106 +1,47 @@
 ﻿#include "FTILuaFuncManager.h"
 
+#include "Buildables/FGBuildableFactoryBuilding.h"
 #include "TweakIt/Logging/FTILog.h"
 
 TMap<FString, FLuaFunc> FTILuaFuncManager::Funcs = {};
 
+FLuaFunc::FLuaFunc()
+{
+	Buf = new TArray<uint8>;
+}
+
 void FLuaFunc::Free()
 {
-	FMemory::Free(Data);
+	delete Buf;
 }
 
 bool FLuaFunc::AddData(const void* NewData, size_t DataSize)
 {
-	LOG("Adding data")
-	bool OK = AddSize(DataSize);
-	LOG("Finished adding size")
-	if (!OK)
-	{
-		LOG("Was not ok. Freeing")
-		FMemory::Free(Data);
-		LOG("Freed")
-		Data = nullptr;
-		return false;
-	}
-	LOG("Copying data")
-	FMemory::Memcpy(Data + Size - DataSize, NewData, DataSize);
-	LOG("Finished")
+	Buf->Append(static_cast<const uint8*>(NewData), DataSize);
 	return true;
 }
 
-const char* FLuaFunc::Read(size_t* OutSize)
+const char* FLuaFunc::GetData()
 {
-	size_t BlockSize = GetNextBlockSize();
-	if (BlockSize == -1)
-	{
-		Reset();
-		OutSize = 0;
-		return nullptr;
-	}
-	*OutSize = BlockSize;
-	uint8* ReturnData = static_cast<uint8*>(FMemory::Malloc(BlockSize));
-	if (ReturnData == nullptr)
-	{
-		MemError = true;
-		return nullptr;
-	}
-	FMemory::Memcpy(ReturnData, Data + DataOffset, BlockSize);
-	DataOffset += BlockSize;
-	return reinterpret_cast<const char*>(ReturnData);
+	return reinterpret_cast<const char*>(Buf->GetData());
 }
 
-size_t FLuaFunc::GetNextBlockSize()
+size_t FLuaFunc::Size()
 {
-	if (BlockSizes.Num() <= BlockIndex + 1)
-	{
-		return -1;
-	}
-	size_t BlockSize = BlockSizes[BlockIndex];
-	BlockIndex++;
-	return BlockSize;
-}
-
-void FLuaFunc::Reset()
-{
-	DataOffset = 0;
-	BlockIndex = 0;
-	MemError = false;
-}
-
-bool FLuaFunc::AddSize(size_t AddedSize)
-{
-	LOG("Adding size")
-	void* New;
-	if (Data == nullptr)
-	{
-		LOG("IS NULL")
-		New = FMemory::Malloc(Size);
-	} else
-	{
-		New = FMemory::Realloc(Data, Size);
-	}
-	LOGF("%llu", AddedSize)
-	if (New == nullptr)
-	{
-		LOG("INVALID ALLOCATION")
-		return false;
-	}
-	Size += AddedSize;
-	BlockSizes.Add(AddedSize);
-	Data = static_cast<uint8*>(New);
-	return true;
+	return Buf->Num();
 }
 
 FLuaFunc FTILuaFuncManager::DumpFunction(lua_State* L, FString Name, int Index, bool Strip)
 {
+	LOGF("Trying to dump %s", *Name)
 	if (!lua_isfunction(L, Index) || lua_iscfunction(L, Index))
 	{
 		luaL_argexpected(L, false, Index, "Lua Function");
 	}
+	LOG("Proceeding with dumping")
 	lua_settop(L, lua_gettop(L) + 1);
 	lua_copy(L, Index, -1);
 	FLuaFunc Desc;
-	LOG("Starting dump")
 	lua_dump(L, WriterFunc, &Desc, Strip);
 	lua_settop(L, -2);
 	Funcs.Add(Name, Desc);
@@ -109,25 +50,12 @@ FLuaFunc FTILuaFuncManager::DumpFunction(lua_State* L, FString Name, int Index, 
 
 int FTILuaFuncManager::LoadFunction(lua_State* L, FLuaFunc Func, FString Name)
 {
-	int r = lua_load(L, ReaderFunc, &Func, TCHAR_TO_UTF8(*Name), nullptr);
-	LOG(r)
-	return 1;
+	LOGF("Loading function %s", *Name)
+	return luaL_loadbuffer(L, Func.GetData(), Func.Size(), TCHAR_TO_UTF8(*Name));
 }
 
 int FTILuaFuncManager::WriterFunc(lua_State* L, const void* NewData, size_t DataSize, void* Descriptor)
 {
-	LOG("Writing")
 	FLuaFunc* Desc = static_cast<FLuaFunc*>(Descriptor);
 	return !Desc->AddData(NewData, DataSize);
-}
-
-const char* FTILuaFuncManager::ReaderFunc(lua_State* L, void* Descriptor, size_t* OutSize)
-{
-	FLuaFunc* Desc = static_cast<FLuaFunc*>(Descriptor);
-	const char* Data = Desc->Read(OutSize);
-	if (Desc->MemError)
-	{
-		luaL_error(L, "Failed to allocate memory in Lua Function Reader");
-	} 
-	return Data;
 }
--- Source/TweakIt/Lua/FTILuaFuncManager.h
+++ Source/TweakIt/Lua/FTILuaFuncManager.h
@@ -2,23 +2,15 @@
 #include "Lua.h"
 
 struct FLuaFunc {
+	FLuaFunc();
+	
 	void Free();
-	
 	bool AddData(const void* NewData, size_t DataSize);
-	const char* Read(size_t* OutSize);
 
-	bool MemError = false;
-	size_t Size = 0;
-	uint8* Data = nullptr;
+	const char* GetData();
+	size_t Size();
 private:
-	size_t GetNextBlockSize();
-	void Reset();
-
-	bool AddSize(size_t Size);
-	
-	int BlockIndex = 0;
-	TArray<size_t> BlockSizes = {};
-	size_t DataOffset = 0;
+	TArray<uint8>* Buf;
 };
 
 class FTILuaFuncManager
@@ -30,7 +22,6 @@ public:
 	static TMap<FString, FLuaFunc> Funcs;
 private:
 	static int WriterFunc(lua_State* L, const void* NewData, size_t DataSize, void* Descriptor);
-	static const char* ReaderFunc(lua_State* L, void* Data, size_t* OutSize);
 };
 
 
